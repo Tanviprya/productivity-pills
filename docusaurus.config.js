eaf311c7f78d54e2a19d46bab642c19d95f01e9b
Tanay Pratap <tanay.mit@gmail.com>
2020-09-01 03:22:21 +0530

    themeing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,59 +1,60 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
+  title: 'the productivity book',
+  tagline: 'just enough productivity funda to get you started',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'tanaypratap', // Usually your GitHub org/user name.
+  projectName: 'theproductivitybook.com', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'theproductivitybook',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'theproductivitybook Logo',
         src: 'img/logo.svg',
       },
       items: [{
           to: 'book/',
           activeBasePath: 'book',
-          label: 'Book',
+          label: 'Read',
           position: 'left',
         },
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
+        // {
+        //   href: 'https://github.com/facebook/docusaurus',
+        //   label: 'GitHub',
+        //   position: 'right',
+        // },
       ],
     },
     footer: {
       style: 'dark',
-      links: [{
-          title: 'Book',
-          items: [{
-            label: 'Style Guide',
-            to: 'Book/',
-          }],
-        },
-        {
-          title: 'Community',
-          items: [{
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
+      links: [
+        // {
+        //   title: 'Book',
+        //   items: [{
+        //     label: 'Style Guide',
+        //     to: 'Book/',
+        //   }],
+        // },
+        // {
+        //   title: 'Community',
+        //   items: [{
+        //       label: 'Stack Overflow',
+        //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+        //     },
+        //     {
+        //       label: 'Discord',
+        //       href: 'https://discordapp.com/invite/docusaurus',
+        //     },
+        //     {
+        //       label: 'Twitter',
+        //       href: 'https://twitter.com/docusaurus',
+        //     },
+        //   ],
+        // },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} The Productivity Book, Built by Tanay Pratap`,
     },
   },
   presets: [
